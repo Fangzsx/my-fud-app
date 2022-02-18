c3b6e25db49c11dc9b7e6c88550fe0bdcb6e527e
jay <jygrzn@gmail.com>
2022-02-18 18:29:34 +0800

    no errors- stable build

--- app/src/main/java/com/fangzsx/my_fud_app/api/MealAPI.kt
+++ app/src/main/java/com/fangzsx/my_fud_app/api/MealAPI.kt
@@ -4,7 +4,6 @@ import com.fangzsx.my_fud_app.models.MealCategoryResponse
 import com.fangzsx.my_fud_app.models.MealResponse
 import com.fangzsx.my_fud_app.models.PopularMealResponse
 import retrofit2.Call
-import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 

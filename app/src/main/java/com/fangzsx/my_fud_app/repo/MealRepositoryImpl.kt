8638db7607b152fd478866ca30f382fe776d3fd3
jay <jygrzn@gmail.com>
2022-02-18 17:47:06 +0800

    add repository implementation

--- app/src/main/java/com/fangzsx/my_fud_app/repo/MealRepositoryImpl.kt
+++ app/src/main/java/com/fangzsx/my_fud_app/repo/MealRepositoryImpl.kt
@@ -1,4 +1,29 @@
 package com.fangzsx.my_fud_app.repo
 
-class MealRepositoryImpl {
+import androidx.lifecycle.LiveData
+import com.fangzsx.my_fud_app.db.MealDao
+import com.fangzsx.my_fud_app.models.Meal
+
+class MealRepositoryImpl(
+    private val dao : MealDao
+) : MealRepository{
+    override fun addMeal(meal: Meal) {
+        dao.addMeal(meal)
+    }
+
+    override fun removeMeal(meal: Meal) {
+        dao.removeMeal(meal)
+    }
+
+    override fun getMeals(): LiveData<List<Meal>> {
+        return dao.getMeals()
+    }
+
+    override fun getMealByName(mealName: String): Meal {
+        return dao.getMealByName(mealName)
+    }
+
+    override fun getMealsByCategory(category: String): LiveData<List<Meal>> {
+        return dao.getMealsByCategory(category)
+    }
 }

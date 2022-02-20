0daff983a56193ac42c859caa9e8e9d23cffd75a
jay <jygrzn@gmail.com>
2022-02-20 10:36:01 +0800

    delete repo, db, di

--- app/src/main/java/com/fangzsx/my_fud_app/ui/fragments/FavoritesFragment.kt
+++ app/src/main/java/com/fangzsx/my_fud_app/ui/fragments/FavoritesFragment.kt
@@ -1,27 +1,20 @@
 package com.fangzsx.my_fud_app.ui.fragments
 
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import com.fangzsx.my_fud_app.R
+import androidx.fragment.app.Fragment
 import com.fangzsx.my_fud_app.databinding.FragmentFavoritesBinding
-import com.fangzsx.my_fud_app.viewmodels.FavoritesViewModel
 
 class FavoritesFragment : Fragment() {
 
     private lateinit var binding : FragmentFavoritesBinding
-    private lateinit var favoritesVM : FavoritesViewModel
-    private val TAG = "FavoritesFragment"
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        favoritesVM = ViewModelProvider(this)[FavoritesViewModel::class.java]
     }
 
     override fun onCreateView(
@@ -34,9 +27,6 @@ class FavoritesFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-
-
 
     }
 }

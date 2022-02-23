a131ecb3bcbcd938eceeaa190ecdab8c801a25fb
jay <jygrzn@gmail.com>
2022-02-23 14:08:09 +0800

    add filter UI for all meals

--- app/src/main/java/com/fangzsx/my_fud_app/adapters/LetterAdapter.kt
+++ app/src/main/java/com/fangzsx/my_fud_app/adapters/LetterAdapter.kt
@@ -1,24 +1,38 @@
 package com.fangzsx.my_fud_app.adapters
 
-import android.view.View
+import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.BaseAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.fangzsx.my_fud_app.databinding.LetterItemBinding
 
 class LetterAdapter : RecyclerView.Adapter<LetterAdapter.ViewHolder>(){
-    inner class ViewHolder(private val binding : LetterItemBinding) : RecyclerView.ViewHolder(binding.root)
+    private var list = ArrayList<Char>()
+
+    inner class ViewHolder(val binding : LetterItemBinding) : RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-
+        return ViewHolder(
+            LetterItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-
+        val letter = list[position]
+        holder.binding.apply {
+            tvLetter.text = letter.toString()
+        }
     }
 
     override fun getItemCount(): Int {
+        return list.size
+    }
 
+    fun setList(list : ArrayList<Char>){
+        this.list = list
     }
 
 }

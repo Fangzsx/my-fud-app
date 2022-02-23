00d2c68655878a740f9db059b8e0a13ae245e757
jay <jygrzn@gmail.com>
2022-02-23 13:34:26 +0800

    add letter layout

---
+++ app/src/main/java/com/fangzsx/my_fud_app/adapters/LetterAdapter.kt
@@ -0,0 +1,24 @@
+package com.fangzsx.my_fud_app.adapters
+
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.fangzsx.my_fud_app.databinding.LetterItemBinding
+
+class LetterAdapter : RecyclerView.Adapter<LetterAdapter.ViewHolder>(){
+    inner class ViewHolder(private val binding : LetterItemBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int {
+
+    }
+
+}
